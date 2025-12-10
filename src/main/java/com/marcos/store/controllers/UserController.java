8ca5124ce1f5353797fd9e408093195679fb355d
xXmarcosX <marcos.v3567@gmail.com>
2025-12-10 13:21:12 -0300

    add: UserController

--- src/main/java/com/marcos/store/controllers/UserController.java
+++ src/main/java/com/marcos/store/controllers/UserController.java
@@ -1,4 +1,19 @@
 package com.marcos.store.controllers;
 
+import com.marcos.store.models.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @GetMapping
+    public ResponseEntity<User> findUser() {
+        User user = new User(1L, "Marcos", "marcos@gmail.com", "1111111", "123456");
+
+        return ResponseEntity.ok().body(user);
+    }
 }
