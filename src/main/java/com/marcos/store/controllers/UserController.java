1237dbb9fdea02f8ade1c438177627fbc775fac3
xXmarcosX <marcos.v3567@gmail.com>
2026-01-08 13:38:19 -0300

    add: Service layer, findUserById

--- src/main/java/com/marcos/store/controllers/UserController.java
+++ src/main/java/com/marcos/store/controllers/UserController.java
@@ -1,18 +1,36 @@
 package com.marcos.store.controllers;
 
 import com.marcos.store.models.User;
+import com.marcos.store.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findUser() {
-        User user = new User(1L, "Marcos", "marcos@gmail.com", "1111111", "123456");
+    public ResponseEntity<List<User>> findUser() {
+        List<User> users = service.findAll();
+
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(
+            @PathVariable
+            Long id
+    ) {
+        User user = service.findById(id);
 
         return ResponseEntity.ok().body(user);
     }
