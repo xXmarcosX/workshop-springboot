5f7baa9b4aa57b600c38f4f912c2e272bbacefc8
xXmarcosX <marcos.v3567@gmail.com>
2026-01-27 19:57:34 -0300

    Product-OrderItem association

--- src/main/java/com/marcos/store/models/Product.java
+++ src/main/java/com/marcos/store/models/Product.java
@@ -1,5 +1,6 @@
 package com.marcos.store.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -27,6 +28,9 @@ public class Product implements Serializable {
             inverseJoinColumns = @JoinColumn(name = "category_id")
     )
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product() {}
 
@@ -80,6 +84,17 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+
+        for (OrderItem item : items) {
+            set.add(item.getOrder());
+        }
+
+        return set;
     }
 
     @Override
