017fdaa01707c362fc06f3cfaadb4160a2358f51
xXmarcosX <marcos.v3567@gmail.com>
2026-01-28 14:42:00 -0300

    add: Insert User

--- src/main/java/com/marcos/store/controllers/UserController.java
+++ src/main/java/com/marcos/store/controllers/UserController.java
@@ -4,11 +4,10 @@ import com.marcos.store.models.User;
 import com.marcos.store.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -33,5 +32,20 @@ public class UserController {
         User user = service.findById(id);
 
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(
+            @RequestBody
+            User user
+    ) {
+        user = service.insert(user);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(user.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(user);
     }
 }
